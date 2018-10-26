bbbe285ec3c306be18ed9e0337f64b49c3fb638d
Magus <magus@bgforge.net>
2018-10-26 18:50:52 +0700

    fix compilation

---
+++ headers/party_op.h
@@ -0,0 +1,56 @@
+// offsets for get_proto_data
+#define PRODATA_PID             (1)
+#define PRODATA_TEXTID          (4)
+
+// items
+#define PRODATA_IT_FLAGS       (24)
+#define PRODATA_IT_TYPE        (32)
+#define PRODATA_IT_MATERIAL   (108)
+#define PRODATA_IT_SIZE       (112)
+#define PRODATA_IT_WEIGHT     (116)
+#define PRODATA_IT_COST       (120)
+
+// weapons
+#define PRODATA_IT_WP_DMG_MIN     (40)
+#define PRODATA_IT_WP_DMG_MAX     (44)
+#define PRODATA_IT_WP_RANGE_MIN   (52)
+#define PRODATA_IT_WP_RANGE_MAX   (56)
+#define PRODATA_IT_WP_MIN_ST      (64)
+#define PRODATA_IT_WP_APCOST_1    (68)
+#define PRODATA_IT_WP_APCOST_2    (72)
+#define PRODATA_IT_WP_BURST       (84)
+#define PRODATA_IT_WP_CALIBER     (88)
+#define PRODATA_IT_WP_AMMO_PID    (92)
+#define PRODATA_IT_WP_MAG_SIZE    (96)
+#define PRODATA_IT_WP_ANIM        (36)
+
+// armor
+#define PRODATA_IT_AR_AC          (36)
+#define PRODATA_IT_AR_DR_NORMAL    (40)
+#define PRODATA_IT_AR_DR_LASER     (44)
+#define PRODATA_IT_AR_DR_FIRE      (48)
+#define PRODATA_IT_AR_DR_PLASMA    (52)
+#define PRODATA_IT_AR_DR_ELECTRICAL   (56)
+#define PRODATA_IT_AR_DR_EMP       (60)
+#define PRODATA_IT_AR_DR_EXPLOSION (64)
+#define PRODATA_IT_AR_DT_NORMAL    (68)
+#define PRODATA_IT_AR_DT_LASER     (72)
+#define PRODATA_IT_AR_DT_FIRE      (76)
+#define PRODATA_IT_AR_DT_PLASMA    (80)
+#define PRODATA_IT_AR_DT_ELECTRICAL   (84)
+#define PRODATA_IT_AR_DT_EMP       (88)
+#define PRODATA_IT_AR_DT_EXPLOSION (92)
+
+// ammo
+#define PRODATA_IT_AM_CALIBER      (36)
+#define PRODATA_IT_AM_PACK_SIZE    (40)
+#define PRODATA_IT_AM_AC_MOD       (44)
+#define PRODATA_IT_AM_DR_MOD       (48)
+#define PRODATA_IT_AM_DMG_MULT     (52)
+#define PRODATA_IT_AM_DMG_DIV      (56)
+
+// critters
+#define PRODATA_CR_CRITTER_FLAGS  (32)
+#define PRODATA_CR_KILL_EXP   (392)
+#define PRODATA_CR_KILL_TYPE  (396)
+
