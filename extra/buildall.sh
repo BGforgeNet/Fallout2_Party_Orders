07ecf6f550e9c92e65c09d04937c393387fcbfc7
Magus <magus@bgforge.net>
2019-07-16 13:27:03 +0700

    autobuild

---
+++ extra/buildall.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -xeu -o pipefail
+
+extra_dir="${extra_dir:-extra}"
+extra_dir="$(realpath $extra_dir)"
+bin_dir="$extra_dir/bin"
+compile="$bin_dir/compile.exe"
+dst="data/scripts"
+mkdir -p "$dst"
+dst="$(realpath $dst)"
+
+mkdir -p external
+cd external
+
+if [[ -d rp ]]; then
+  cd rp
+  git pull
+  cd ..
+else
+  git clone https://github.com/BGforgeNet/Fallout2_Restoration_Project.git rp
+fi
+
+if [[ -d fo2tweaks ]]; then
+  cd fo2tweaks
+  git pull
+  cd ..
+else
+  git clone https://github.com/BGforgeNet/FO2tweaks.git fo2tweaks
+fi
+
+if [[ -d sfall ]]; then
+  cd sfall
+  git pull
+  cd ..
+else
+  git clone https://github.com/phobos2077/sfall.git sfall
+fi
+
+cd ..
+
+files=$(cat $extra_dir/build.list)
+mkdir -p $dst
+cd source
+for f in $files; do
+  int_name="$(echo $f | tr "[A-Z]" "[a-z]" | sed 's|\.ssl$|.int|')"
+  wine $compile -l -O2 -p -s -q -n "$f" -o "$dst/$int_name"
+done
