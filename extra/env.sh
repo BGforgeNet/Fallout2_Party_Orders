19ce3907f86b19177bc6c22a2cacdde028388af2
Magus <magus@bgforge.net>
2026-08-15 11:01:04 +0700

    Update actions

--- extra/env.sh
+++ extra/env.sh
@@ -6,9 +6,9 @@ extra_dir="$(realpath extra)"
 export extra_dir
 export bin_dir="$extra_dir/bin"
 export mods_dir="mods"
-export mpack_version=${mpack_version:-4.3}
+export mpack_version=${mpack_version:-4.5}
 export mpack_7z="mpack.7z"
-export sfall_version=${sfall_version:-4.3}
+export sfall_version=${sfall_version:-4.5}
 export WINEARCH=win32
 export WINEDEBUG=-all
 export mod_name=party_orders
